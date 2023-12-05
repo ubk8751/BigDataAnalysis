492a6dd1df291472b19362d028cd7723846bf311
ubk8751 <ubk8751@gmail.com>
2023-12-05 22:48:21 +0100

    the beginning of The great Sam's changes

--- Containers/CodeStreamConsumer/src/CloneDetector.js
+++ Containers/CodeStreamConsumer/src/CloneDetector.js
@@ -79,59 +79,89 @@ class CloneDetector {
         return match;
     }
 
+    // TODO
+    // For each chunk in file.chunks, find all #chunkMatch() in compareFile.chunk
+    // For each matching chunk, create a new Clone.
+    // Store the resulting (flat) array in file.instances.
+    // 
+    // TIP 1: Array.filter to find a set of matches, Array.map to return a new array with modified objects.
+    // TIP 2: You can daisy-chain calls to filter().map().filter().flat() etc.
+    // TIP 3: Remember that file.instances may have already been created, so only append to it.
+    //
+    // Return: file, including file.instances which is an array of Clone objects (or an empty array).
+    //
     #filterCloneCandidates(file, compareFile) {
-        // TODO
         // For each chunk in file.chunks, find all #chunkMatch() in compareFile.chunk
         // For each matching chunk, create a new Clone.
         // Store the resulting (flat) array in file.instances.
-        // 
-        // TIP 1: Array.filter to find a set of matches, Array.map to return a new array with modified objects.
-        // TIP 2: You can daisy-chain calls to filter().map().filter().flat() etc.
-        // TIP 3: Remember that file.instances may have already been created, so only append to it.
-        //
-        // Return: file, including file.instances which is an array of Clone objects (or an empty array).
-        //
-
-        file.instances = file.instances || [];        
+        const newInstances = file.chunks.flatMap(chunk => {
+            const matchingChunks = compareFile.chunks.filter(compareChunk => this.#chunkMatch(chunk, compareChunk));
+            return matchingChunks.map(matchingChunk => new Clone(chunk, matchingChunk, file.name, compareFile.name));
+        });
+        
+        file.instances = file.instances || [];
         file.instances = file.instances.concat(newInstances);
         return file;
     }
-     
+
+    // TODO
+    // For each Clone in file.instances, try to expand it with every other Clone
+    // (using Clone::maybeExpandWith(), which returns true if it could expand)
+    // 
+    // Comment: This should be doable with a reduce:
+    //          For every new element, check if it overlaps any element in the accumulator.
+    //          If it does, expand the element in the accumulator. If it doesn't, add it to the accumulator.
+    //
+    // ASSUME: As long as you traverse the array file.instances in the "normal" order, only forward expansion is necessary.
+    // 
+    // Return: file, with file.instances only including Clones that have been expanded as much as they can,
+    //
+    //         and not any of the Clones used during that expansion.
+    //
     #expandCloneCandidates(file) {
-        // TODO
         // For each Clone in file.instances, try to expand it with every other Clone
         // (using Clone::maybeExpandWith(), which returns true if it could expand)
-        // 
-        // Comment: This should be doable with a reduce:
-        //          For every new element, check if it overlaps any element in the accumulator.
-        //          If it does, expand the element in the accumulator. If it doesn't, add it to the accumulator.
-        //
-        // ASSUME: As long as you traverse the array file.instances in the "normal" order, only forward expansion is necessary.
-        // 
-        // Return: file, with file.instances only including Clones that have been expanded as much as they can,
-        //         and not any of the Clones used during that expansion.
-        //
-
+        const expandedClones = file.instances.reduce((acc, currentClone) => {
+            const cloneExpanded = acc.some(existingClone => existingClone.maybeExpandWith(currentClone));
+            if (!cloneExpanded) {
+                acc.push(currentClone);
+            }
+            return acc;
+        }, []);
+    
+        file.instances = expandedClones;
         return file;
     }
-    
+
+    // TODO
+    // For each clone, accumulate it into an array if it is new
+    // If it isn't new, update the existing clone to include this one too
+    // using Clone::addTarget()
+    // 
+    // TIP 1: Array.reduce() with an empty array as start value.
+    //        Push not-seen-before clones into the accumulator
+    // TIP 2: There should only be one match in the accumulator
+    //        so Array.find() and Clone::equals() will do nicely.
+    //
+    // Return: file, with file.instances containing unique Clone objects that may contain several targets
+    //
     #consolidateClones(file) {
-        // TODO
         // For each clone, accumulate it into an array if it is new
         // If it isn't new, update the existing clone to include this one too
         // using Clone::addTarget()
-        // 
-        // TIP 1: Array.reduce() with an empty array as start value.
-        //        Push not-seen-before clones into the accumulator
-        // TIP 2: There should only be one match in the accumulator
-        //        so Array.find() and Clone::equals() will do nicely.
-        //
-        // Return: file, with file.instances containing unique Clone objects that may contain several targets
-        //
-
+        const uniqueClones = file.instances.reduce((acc, currentClone) => {
+            const existingClone = acc.find(clone => clone.equals(currentClone));
+            if (existingClone) {
+                existingClone.addTarget(currentClone.targets[0]);
+            } else {
+                acc.push(currentClone);
+            }
+            return acc;
+        }, []);
+    
+        file.instances = uniqueClones;
         return file;
     }
-    
 
     // Public Processing Steps
     // --------------------
@@ -156,25 +186,34 @@ class CloneDetector {
     matchDetect(file) {
         let allFiles = this.#myFileStore.getAllFiles();
         file.instances = file.instances || [];
+    
         for (let f of allFiles) {
-            // TODO implement these methods (or re-write the function matchDetect() to your own liking)
-            // 
-            // Overall process:
-            // 
-            // 1. Find all equal chunks in file and f. Represent each matching pair as a Clone.
-            //
-            // 2. For each Clone with endLine=x, merge it with Clone with endLine-1=x
-            //    remove the now redundant clone, rinse & repeat.
-            //    note that you may end up with several "root" Clones for each processed file f
-            //    if there are more than one clone between the file f and the current
-            //
-            // 3. If the same clone is found in several places, consolidate them into one Clone.
-            //
-            file = this.#filterCloneCandidates(file, f); 
-            file = this.#expandCloneCandidates(file);
-            file = this.#consolidateClones(file); 
+            // Find all equal chunks in file and f. Represent each matching pair as a Clone.
+            file = this.#filterCloneCandidates(file, f);
+    
+            // For each Clone with endLine=x, merge it with Clone with endLine-1=x
+            // remove the now redundant clone, rinse & repeat.
+            for (let i = 0; i < file.instances.length; i++) {
+                let currentClone = file.instances[i];
+                if (currentClone.endLine > 0) {
+                    for (let j = 0; j < file.instances.length; j++) {
+                        if (i !== j) {
+                            let otherClone = file.instances[j];
+                            if (currentClone.endLine - 1 === otherClone.endLine && currentClone.equalsExceptEndLine(otherClone)) {
+                                currentClone.mergeWith(otherClone);
+                                file.instances.splice(j, 1);
+                                i--; // Adjust the index as we removed an element
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+    
+            // If the same clone is found in several places, consolidate them into one Clone.
+            file = this.#consolidateClones(file);
         }
-
+    
         return file;
     }
 
--- Containers/CodeStreamConsumer/src/index.js
+++ Containers/CodeStreamConsumer/src/index.js
@@ -8,7 +8,20 @@ const Timer = require('./Timer');
 const CloneDetector = require('./CloneDetector');
 const CloneStorage = require('./CloneStorage');
 const FileStorage = require('./FileStorage');
+const fileTimers = [];
+const TIMERS_FREQ = 10;
+const STATS_FREQ = 100;
+const URL = process.env.URL || 'http://localhost:8080/';
+var lastFile = null;
 
+/*
+1. Preprocessing: Remove uninteresting code, determine source and comparison units/granularities
+2. Transformation: One or more extraction and/or transformation techniques are applied to the preprocessed code to obtain an intermediate representation of the code.
+3. Match Detection: Transformed units (and/or metrics for those units) are compared to find similar source units.
+4. Formatting: Locations of identified clones in the transformed units are mapped to the original code base by file location and line number.
+5. Post-Processing and Filtering: Visualisation of clones and manual analysis to filter out false positives
+6. Aggregation: Clone pairs are aggregated to form clone classes or families, in order to reduce the amount of data and facilitate analysis.
+*/
 
 // Express and Formidable stuff to receice a file for further processing
 // --------------------
@@ -23,7 +36,24 @@ function fileReceiver(req, res, next) {
     return res.end('');
 }
 
+function viewTimers(req, res, next) {
+    let page = '<HTML><HEAD><TITLE>CodeStream Clone Detector Timers</TITLE></HEAD>\n';
+    page += '<BODY><H1>CodeStream Clone Detector Timers</H1>\n';
+    
+    // Display timers and statistics
+    fileTimers.forEach((fileTimer, index) => {
+        page += `<h2>File ${index + 1}: ${fileTimer.filename}</h2>\n`;
+        for (let timer in fileTimer.timers) {
+            page += `<p>${timer}: ${fileTimer.timers[timer] / 1000n} µs</p>\n`;
+        }
+    });
+    
+    page += '</BODY></HTML>';
+    res.send(page);
+}
+
 app.get('/', viewClones );
+app.get('/timers', viewTimers);
 
 const server = app.listen(PORT, () => { console.log('Listening for files on port', PORT); });
 
@@ -52,22 +82,41 @@ function listClonesHTML() {
     let cloneStore = CloneStorage.getInstance();
     let output = '';
 
-    cloneStore.clones.forEach( clone => {
+    cloneStore.clones.forEach(clone => {
         output += '<hr>\n';
-        output += '<h2>Source File: ' + clone.sourceName + '</h2>\n';
-        output += '<p>Starting at line: ' + clone.sourceStart + ' , ending at line: ' + clone.sourceEnd + '</p>\n';
-        output += '<ul>';
-        clone.targets.forEach( target => {
-            output += '<li>Found in ' + target.name + ' starting at line ' + target.startLine + '\n';            
-        });
-        output += '</ul>\n'
-        output += '<h3>Contents:</h3>\n<pre><code>\n';
-        output += clone.originalCode;
-        output += '</code></pre>\n';
+        if (Array.isArray(clone.sourceName) && clone.sourceName.length > 0) {
+            const firstSourceLine = clone.sourceName[0].myLineNumber;
+            const lastSourceLine = clone.sourceName[clone.sourceName.length - 1].myLineNumber;
+            console.log(clone.sourceName[clone.sourceName.length - 1], lastSourceLine)
+            output += '<h2>Source File: ' + clone.sourceChunk + '</h2>\n';
+            output += '<p>Starting at line: ' + firstSourceLine + ' , ending at line: ' + lastSourceLine + '</p>\n';
+            output += '<ul>';
+            clone.targets.forEach(target => {
+                if (target && Array.isArray(target.name) && target.name.length > 0) {
+                    // Assuming the first target line is representative
+                    const firstTargetLine = target.name[0];
+                    if (firstTargetLine && firstTargetLine.myContent) {
+                        output += '<li>Found in ' + firstTargetLine.myContent + ' starting at line ' + target.startLine + '\n';
+                    } else {
+                        output += '<li>Found in unknown location starting at line ' + target.startLine + '\n';
+                    }
+                } else {
+                    output += '<li>Found in unknown location starting at line ' + target.startLine + '\n';
+                }
+            });
+            
+            output += '</ul>\n';
+            output += '<h3>Contents:</h3>\n<pre><code>\n';
+            output += clone.originalCode;
+            output += '</code></pre>\n';
+        } else {
+            console.error('Error: sourceName is not an array or is empty in the clone object.');
+        }
     });
 
     return output;
 }
+
 
 function listProcessedFilesHTML() {
     let fs = FileStorage.getInstance();
@@ -103,20 +152,24 @@ PASS = fn => d => {
     }
 };
 
-const STATS_FREQ = 100;
-const URL = process.env.URL || 'http://localhost:8080/';
-var lastFile = null;
 
-function maybePrintStatistics(file, cloneDetector, cloneStore) {
-    if (0 == cloneDetector.numberOfProcessedFiles % STATS_FREQ) {
+function printStatistics(file, cloneDetector, cloneStore) {
+    if (cloneDetector.numberOfProcessedFiles % STATS_FREQ === 0) {
         console.log('Processed', cloneDetector.numberOfProcessedFiles, 'files and found', cloneStore.numberOfClones, 'clones.');
-        let timers = Timer.getTimers(file);
-        let str = 'Timers for last file processed: ';
-        for (t in timers) {
-            str += t + ': ' + (timers[t] / (1000n)) + ' µs '
-        }
-        console.log(str);
         console.log('List of found clones available at', URL);
+        // Calculate and display more in-depth statistics on the /timers page
+        /*if (cloneDetector.numberOfProcessedFiles % TIMERS_FREQ === 0) {
+            let avgTimePerFile = fileTimers.reduce((sum, timer) => sum + Number(Object.values(timer.timers)[0]), 0) / fileTimers.length;
+            let avgTimeLast100Files = fileTimers.slice(-100).reduce((sum, timer) => sum + Number(Object.values(timer.timers)[0]), 0) / Math.min(fileTimers.length, 100);
+            let avgTimeLast1000Files = fileTimers.slice(-1000).reduce((sum, timer) => sum + Number(Object.values(timer.timers)[0]), 0) / Math.min(fileTimers.length, 1000);
+        
+            console.log('Average time per file:', avgTimePerFile, 'µs');
+            console.log('Average time per last 100 files:', avgTimeLast100Files, 'µs');
+            console.log('Average time per last 1000 files:', avgTimeLast1000Files, 'µs');
+            // Add more statistics as needed
+        
+            console.log('List of found clones available at', URL);
+        }*/
     }
 
     return file;
@@ -124,37 +177,28 @@ function maybePrintStatistics(file, cloneDetector, cloneStore) {
 
 // Processing of the file
 // --------------------
+// TODO Store the timers from every file (or every 10th file), create a new landing page /timers
+// and display more in-depth statistics there. Examples include:
+// average times per file, average times per last 100 files, last 1000 files.
+// Perhaps throw in a graph over all files.
+// .catch(console.log);
 function processFile(filename, contents) {
     let cd = new CloneDetector();
     let cloneStore = CloneStorage.getInstance();
+    let file = { name: filename, contents: contents };
 
-    return Promise.resolve({name: filename, contents: contents} )
-        //.then( PASS( (file) => console.log('Processing file:', file.name) ))
-        .then( (file) => Timer.startTimer(file, 'total') )
-        .then( (file) => cd.preprocess(file) )
-        .then( (file) => cd.transform(file) )
+    return Promise.resolve(file)
+        .then((file) => Timer.startTimer(file, 'total'))
+        .then((file) => cd.preprocess(file))
+        .then((file) => cd.transform(file))
+        .then((file) => Timer.startTimer(file, 'match'))
+        .then((file) => cd.matchDetect(file))
+        .then((file) => cloneStore.storeClones(file))
+        .then((file) => Timer.endTimer(file, 'match'))
+        .then((file) => cd.storeFile(file))
+        .then((file) => Timer.endTimer(file, 'total'))
+        .then(PASS((file) => lastFile = file))
+        .then(PASS((file) => printStatistics(file, cd, cloneStore)))
+        .then((file) => { file }); // Return the file for further processing or logging
+}
 
-        .then( (file) => Timer.startTimer(file, 'match') )
-        .then( (file) => cd.matchDetect(file) )
-        .then( (file) => cloneStore.storeClones(file) )
-        .then( (file) => Timer.endTimer(file, 'match') )
-
-        .then( (file) => cd.storeFile(file) )
-        .then( (file) => Timer.endTimer(file, 'total') )
-        .then( PASS( (file) => lastFile = file ))
-        .then( PASS( (file) => maybePrintStatistics(file, cd, cloneStore) ))
-    // TODO Store the timers from every file (or every 10th file), create a new landing page /timers
-    // and display more in depth statistics there. Examples include:
-    // average times per file, average times per last 100 files, last 1000 files.
-    // Perhaps throw in a graph over all files.
-        .catch( console.log );
-};
-
-/*
-1. Preprocessing: Remove uninteresting code, determine source and comparison units/granularities
-2. Transformation: One or more extraction and/or transformation techniques are applied to the preprocessed code to obtain an intermediate representation of the code.
-3. Match Detection: Transformed units (and/or metrics for those units) are compared to find similar source units.
-4. Formatting: Locations of identified clones in the transformed units are mapped to the original code base by file location and line number.
-5. Post-Processing and Filtering: Visualisation of clones and manual analysis to filter out false positives
-6. Aggregation: Clone pairs are aggregated to form clone classes or families, in order to reduce the amount of data and facilitate analysis.
-*/
