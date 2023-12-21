40682925a2b430da6e0d2bca9ea4c0d73ed96bb0
ubk8751 <ubk8751@gmail.com>
2023-12-21 18:48:43 +0100

    It prints correctly

--- Containers/MonitorTool/src/monitortool.py
+++ Containers/MonitorTool/src/monitortool.py
@@ -10,12 +10,29 @@ mongo_dbname = "cloneDetector"
 client = pymongo.MongoClient(mongo_host, mongo_port)
 db = client[mongo_dbname]
 
+# Initialize lists to store processing times
+files_processing_times = []
+chunks_processing_times = []
+candidates_processing_times = []
+clones_processing_times = []
+
 while True:
     try:
+        start_time = time.time()
+
         files_count = db["files"].count_documents({})
         chunks_count = db["chunks"].count_documents({})
         candidates_count = db["candidates"].count_documents({})
         clones_count = db["clones"].count_documents({})
+
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        # Append processing times to respective lists
+        files_processing_times.append((files_count, elapsed_time))
+        chunks_processing_times.append((chunks_count, elapsed_time))
+        candidates_processing_times.append((candidates_count, elapsed_time))
+        clones_processing_times.append((clones_count, elapsed_time))
 
         print("Files Count:", files_count)
         print("Chunks Count:", chunks_count)
@@ -27,9 +44,24 @@ while True:
             print("New Status Update:")
             print("Timestamp:", update["timestamp"])
             print("Message:", update["message"])
-            print("----------------------------")
+        # Calculate and print statistics every 30 minutes (adjust as needed)
+        if len(files_processing_times) > 0 and len(files_processing_times) % (1 * 60 / 10) == 0:
+            calculate_and_print_statistics(files_processing_times, "Files")
+            calculate_and_print_statistics(chunks_processing_times, "Chunks")
+            calculate_and_print_statistics(candidates_processing_times, "Candidates")
+            calculate_and_print_statistics(clones_processing_times, "Clones")
+        print("----------------------------")
         time.sleep(10)
+
 
     except Exception as e:
         print("Error:", str(e))
         time.sleep(10)
+
+
+def calculate_and_print_statistics(processing_times, unit_type):
+    if processing_times:
+        counts, times = zip(*processing_times)
+        mean_time = sum(times) / len(times)
+        print(f"Mean processing time for {unit_type}: {mean_time} seconds")
+
