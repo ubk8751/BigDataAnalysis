45d7044944aaa18697a18c0e5013ae9d4af8d487
ubk8751 <ubk8751@gmail.com>
2023-12-25 09:34:55 +0100

    -u flag bs works

--- boot.sh
+++ boot.sh
@@ -9,15 +9,22 @@ help() {
     echo "  c: Run the docker-compose down command."
     echo "  v: Include the '-v' flag in docker-compose down."
     echo "  i: Include the '--rmi all' flag in docker-compose down."
+    echo "     If used you will need to rebuild the images monitortool and"
+    echo "     clone-detector locally"
     echo "  p: Include 'p' to run docker system prune."
     echo "  b: Include the '--build' option when running docker-compose up."
+    echo "  u: Include the -u flag when building the monitortoolcontainer."
+    echo "     Activates 'flush' and deactivates stdin/stdout buffering."
     exit 1
 }
 
 options=""
 boptions=""
 prune=""
+u_flag=""
 use_c=false
+p_include_u=false
+
 
 args="$@"
 
@@ -45,11 +52,18 @@ for ((i = 0; i < ${#args}; i++)); do
         b)
             boptions="$boptions --build"
             ;;
+        u)  # Remove the * before -u
+            p_include_u=true
+            ;;
         *)
             help "Invalid option: $letter"
             ;;
     esac
 done
+
+if [ "$p_include_u" = true ]; then
+    u_flag="-u"
+fi
 
 if [ -n "$use_c" ]; then
     docker-compose -f all-at-once.yaml down $options
@@ -58,5 +72,7 @@ fi
 if [ -n "$prune" ]; then
     docker system prune -f
 fi
+
+export FLAGS="$u_flag"
 
 docker-compose -f all-at-once.yaml up $boptions
